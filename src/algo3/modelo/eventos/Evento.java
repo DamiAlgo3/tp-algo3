9440c5b627035272415ba992b02d56ba2a84fd92
Francisco Strambini <franfiuba@gmail.com>
2013-11-10 19:58:31 -0200

    Update Evento.java

--- src/algo3/modelo/eventos/Evento.java
+++ src/algo3/modelo/eventos/Evento.java
@@ -1,15 +1,3 @@
 package eventos;
-import vehiculos.Auto;
-import vehiculos.Moto;
-import vehiculos.TodoTerreno;
 
-
-public interface Evento {
-	
-	public void resolver(Moto unaMoto);
-
-	public void resolver(TodoTerreno unTodoTerreno);	
-	
-	public void resolver(Auto unAuto);
-
-}
+public interface Evento {}

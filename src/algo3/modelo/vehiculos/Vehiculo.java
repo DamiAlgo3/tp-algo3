906ec3ee2948a09506fce72cdf0c32c66d089ee6
Francisco Strambini <franfiuba@gmail.com>
2013-11-10 20:01:16 -0200

    Update Vehiculo.java

--- src/algo3/modelo/vehiculos/Vehiculo.java
+++ src/algo3/modelo/vehiculos/Vehiculo.java
@@ -6,9 +6,6 @@ public abstract class Vehiculo {
 	
 	private int movimientos;
 	
-	public int getMovimientos(){return movimientos;}
-	public void setMovimientos(int movimientos){this.movimientos = movimientos;}
-	
-	public abstract void resolverEvento(Evento unEvento);
+	public abstract void interactuar(Evento unEvento);
 	
 	}

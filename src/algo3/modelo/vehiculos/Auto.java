1819de6aa1b2e447b801463c3bcf1b58542412c0
Francisco Strambini <franfiuba@gmail.com>
2013-11-11 04:43:07 -0200

    Update Auto.java

--- src/algo3/modelo/vehiculos/Auto.java
+++ src/algo3/modelo/vehiculos/Auto.java
@@ -6,10 +6,7 @@ public class Auto extends Vehiculo {
 	public void interactuar(Bache unBache){movimientos += 3;}
 	public void interactuar(Piquete unPiquete){}
 	public void interactuar(ControlPolicial unControlPolicial){
-		
 		int aux = (int) (Math.random()*100);
 		if(aux < 50) { movimientos += 3;}
 		}
-	
-	
 	}

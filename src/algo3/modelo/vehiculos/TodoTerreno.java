5f94c2bcad2d2cebce132b4c68818b2ac711a1e0
Francisco Strambini <franfiuba@gmail.com>
2013-11-11 04:43:55 -0200

    Update TodoTerreno.java

--- src/algo3/modelo/vehiculos/TodoTerreno.java
+++ src/algo3/modelo/vehiculos/TodoTerreno.java
@@ -7,9 +7,7 @@ public class TodoTerreno extends Vehiculo {
 	public void interactuar(Bache unBache){}
 	public void interactuar(Piquete unPiquete){}
 	public void interactuar(ControlPolicial unControlPolicial){
-		
 		int aux = (int) (Math.random()*100);
 		if(aux < 30) { movimientos += 3;}
+		}
 	}
-	
-}
